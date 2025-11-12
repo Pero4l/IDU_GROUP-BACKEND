4d1312d7136cf14821f3844f88e30bb1abd6e0be
pero4l <lilpero03@gmail.com>
2025-11-12 21:05:50 +0100

    feat:builded the server and about to test it

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
+app.get("/", (req, res) =>{
+    res.status(200).json({
+        "success": false,
+        "message": "WELCOME TO IDU_GROUP"
+    })
+});
+
+const PORT = process.env.PORT || 5500
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
