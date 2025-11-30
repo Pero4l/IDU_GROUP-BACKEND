a3fb9e40d217c9c18150023712a8ae3ff80e5cbd
pero4l <lilpero03@gmail.com>
2025-11-30 22:20:59 +0100

    feat: finished setting up the backend to run

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
-// const {sequelize, Sequelize} = require('sequelize')
-// require('dotenv').config();
+const {sequelize, Sequelize} = require('sequelize')
+require('dotenv').config();
 
 
-// const connection = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-//     host: "127.0.0.1",
-//     dialect: "mysql"
-// })
-// module.exports = connection;
+const connection = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+    host: "127.0.0.1",
+    dialect: "mysql"
+})
+module.exports = connection;
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) =>{
 
 
 // DB CONNECTION
-const PORT = process.env.PORT || 5500
+const PORT = process.env.PORT || 9300
 
 db.sync({ force: false, alter: false })
   .then(async () => {
