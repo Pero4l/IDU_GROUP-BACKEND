4022a7a2e3c9e52b4d52f5329defd67e03cd9a45
pero4l <lilpero03@gmail.com>
2025-11-30 22:04:46 +0100

    feat: still setting up the backend

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 
 const app = express();
 
+const db = require("./config/db");
+
 app.get("/", (req, res) =>{
     res.status(200).json({
         "success": false,
@@ -11,8 +13,21 @@ app.get("/", (req, res) =>{
     })
 });
 
+
+
+
+// DB CONNECTION
 const PORT = process.env.PORT || 5500
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-})
+db.sync({ force: false, alter: false })
+  .then(async () => {
+    
+    app.listen(PORT, () => {
+      console.log(
+        `✅ Database connected successfully and Server running on PORT:${PORT}`
+      );
+    });
+  })
+  .catch((e) => {
+    console.log(`❌ Database connection failed:`, e);
+  });
