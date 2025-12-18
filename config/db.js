8bff96dc8974910c649ff3e51d492a6f40c1dfe3
Promise278 <promiseobi2007@gmail.com>
2025-12-18 23:51:59 +0100

    hosted database and added connection

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
-const {sequelize, Sequelize} = require('sequelize')
-require('dotenv').config();
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
+const connection = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: console.log,
+});
 
-const connection = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-    host: "127.0.0.1",
-    dialect: "mysql"
-})
 module.exports = connection;
