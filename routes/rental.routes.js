200d1480ce857e63a4325d9ebe03fc1db5cd66e1
pero4l <lilpero03@gmail.com>
2026-03-18 23:51:39 +0100

    add auth

--- routes/rental.routes.js
+++ routes/rental.routes.js
@@ -5,8 +5,8 @@ const { authMiddleware } = require('../middleware/authUserMiddleware');
 const { addRental, seeAllRentals, getRental, updateRental, deleteRental } = require('../controllers/rental.controller');
 
 router.post('/post', authMiddleware, addRental);
-router.get('/all', seeAllRentals);
-router.get('/get1/:id', getRental);
+router.get('/all', authMiddleware, seeAllRentals);
+router.get('/get1/:id', authMiddleware, getRental);
 router.put('/update/:id', authMiddleware, updateRental);
 router.delete('/delete/:id', authMiddleware, deleteRental);
 
