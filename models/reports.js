3cca5c17d3b1b3d59deb4bfd3d07989683c9a413
pero4l <lilpero03@gmail.com>
2026-03-31 23:00:03 +0100

    feat: creating relationships

--- models/reports.js
+++ models/reports.js
@@ -11,6 +11,15 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Reports.belongsTo(models.Users, {
+        foreignKey: "user_id",
+        as: "user"
+      });
+
+      Reports.belongsTo(models.Users, {
+        foreignKey: "report_user_id",
+        as: "report_user"
+      });
     }
   }
   Reports.init({
