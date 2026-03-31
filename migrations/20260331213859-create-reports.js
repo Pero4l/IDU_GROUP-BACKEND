e6a435e1a8758562c3f394182342be987248d255
pero4l <lilpero03@gmail.com>
2026-03-31 22:42:09 +0100

    feat: creating the table

---
+++ migrations/20260331213859-create-reports.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('reports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.UUID
+      },
+      report_user_id: {
+        type: Sequelize.UUID
+      },
+      report_name: {
+        type: Sequelize.STRING
+      },
+      report_type: {
+        type: Sequelize.STRING
+      },
+      report_message: {
+        type: Sequelize.STRING
+      },
+      report_status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('reports');
+  }
+};
---
+++ models/reports.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class reports extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  reports.init({
+    user_id: DataTypes.UUID,
+    report_user_id: DataTypes.UUID,
+    report_name: DataTypes.STRING,
+    report_type: DataTypes.STRING,
+    report_message: DataTypes.STRING,
+    report_status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'reports',
+  });
+  return reports;
+};
