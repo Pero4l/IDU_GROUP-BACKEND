d4df1b0487f57b6f9d6c8a093262975648bcbe95
pero4l <lilpero03@gmail.com>
2026-03-31 22:46:33 +0100

    feat: editing the table

--- migrations/20260331213859-create-reports.js
+++ migrations/20260331213859-create-reports.js
@@ -18,6 +18,9 @@ module.exports = {
       report_name: {
         type: Sequelize.STRING
       },
+      report_number: {
+        type: Sequelize.STRING
+      },
       report_type: {
         type: Sequelize.STRING
       },
