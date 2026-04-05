3f38912148b86968625bf477d2f50f80938f9d30
pero4l <lilpero03@gmail.com>
2026-04-05 23:40:14 +0100

    feat: search rentals

--- routes/rental.routes.js
+++ routes/rental.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const { authMiddleware } = require('../middleware/authUserMiddleware');
-const { addRental, seeAllRentals, getRental, updateRental, deleteRental } = require('../controllers/rental.controller');
+const { getLocationMiddleware } = require('../middleware/getLocationMiddleware');
+const { addRental, seeAllRentals, getRental, updateRental, deleteRental, searchRentals } = require('../controllers/rental.controller');
 
 router.post('/post', authMiddleware, addRental);
 router.get('/all', authMiddleware, seeAllRentals);
 router.get('/get1/:id', authMiddleware, getRental);
 router.put('/update/:id', authMiddleware, updateRental);
 router.delete('/delete/:id', authMiddleware, deleteRental);
+router.get('/search', authMiddleware, getLocationMiddleware, searchRentals);
 
 module.exports = router;
